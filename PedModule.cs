90e2dc300035f2a7d7412cd2bc498d17a22eef2a
jefferson.fernandes <jefferson.fernandes@greensolucoes.com.br>
2025-03-08 03:40:52 -0300

    Minimally functional version. Needs memory GC solution

--- PedModule.cs
+++ PedModule.cs
@@ -10,19 +10,13 @@ namespace VRoguePed
 {
     internal class PedModule
     {
-        private static Ped roguePed;
-        private static Ped victimPed;
-        private static Blip rogueBlip;
-        private static Blip victimBlip;
-        private static Vehicle targetVehicle;
-        private static TaskSequence taskSequence;
         private static Vector3 airportEntracePosition = new Vector3(-1337.0f, -3044.0f, 13.9f);
 
         public static void MakePedGoRogueProc()
         {
             try
             {
-                roguePed = World.GetNearbyPeds(Game.Player.Character, 40f)
+                Ped roguePed = World.GetNearbyPeds(Game.Player.Character, 40f)
                      .Where(p => p != null
                         && p.Exists()
                         && p != Game.Player.Character
@@ -39,7 +33,7 @@ namespace VRoguePed
                     return;
                 }
 
-                victimPed = World.GetNearbyPeds(roguePed, 40f)
+                Ped victimPed = World.GetNearbyPeds(roguePed, 40f)
                     .Where(p => p != null
                     && p.Exists()
                     && p != roguePed
@@ -59,35 +53,26 @@ namespace VRoguePed
 
                 roguePed.Weapons.Give(GTA.Native.WeaponHash.Pistol50, 99, true, true);
 
-                //rogueBlip = roguePed.AddBlip();
-                //rogueBlip.IsFlashing = true;
-                //rogueBlip.Color = BlipColor.Green;
-                //rogueBlip.CategoryType = BlipCategoryType.DistanceShown;
-                //rogueBlip.DisplayType = BlipDisplayType.BothMapNoSelectable;
-                //rogueBlip.NumberLabel = 1;
-                //rogueBlip.Name = "RoguePed";
+                TaskSequence taskSequence = new TaskSequence();
 
-                //victimBlip = victimPed.AddBlip();
-                //victimBlip.IsFlashing = true;
-                //victimBlip.Color = BlipColor.Blue;
-                //victimBlip.CategoryType = BlipCategoryType.DistanceShown;
-                //victimBlip.DisplayType = BlipDisplayType.BothMapNoSelectable;
-                //victimBlip.NumberLabel = 2;
-                //victimBlip.Name = "VictimPed";
-
-                taskSequence = new TaskSequence();
-
-                taskSequence.AddTask.ClearAll();
-                taskSequence.AddTask.LookAt(victimPed, 4000);
+                taskSequence.AddTask.ClearAllImmediately();
+                taskSequence.AddTask.LookAt(victimPed, 5000);
                 taskSequence.AddTask.RunTo(victimPed.Position, false);
+                taskSequence.AddTask.StandStill(3000);
                 taskSequence.AddTask.AimAt(victimPed, 1000);
-                taskSequence.AddTask.ShootAt(victimPed, 5000);
-                taskSequence.AddTask.EnterVehicle();
-                //taskSequence.AddTask.DriveTo(roguePed.CurrentVehicle, airportEntracePosition, 30f, VehicleDrivingFlags.DrivingModePloughThrough, 50f);
-                //taskSequence.Close();
+                taskSequence.AddTask.ShootAt(victimPed, 6000);
+
+                Vehicle rogueVehicle = VehicleUtil.GetNearesVehicle(roguePed);
+
+                if (rogueVehicle != null && rogueVehicle.Exists() && rogueVehicle.IsDriveable)
+                {
+                    taskSequence.AddTask.RunTo(rogueVehicle.Position, false);
+                    taskSequence.AddTask.EnterVehicle(rogueVehicle, VehicleSeat.Driver);
+                    taskSequence.AddTask.DriveTo(rogueVehicle, airportEntracePosition, 30f, 15f, (int)DrivingStyle.Rushed);
+                }
+                taskSequence.Close();
 
                 roguePed.Task.PerformSequence(taskSequence);
-
                 Util.Subtitle("MakePedGoRogueProc() called.", 1700);
             }
             catch (Exception e)
---
+++ VehicleUtil.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GTA;
+
+namespace VRoguePed
+{
+    internal class VehicleUtil
+    {
+        public static Vehicle GetNearesVehicle(Ped ped)
+        {
+            Vehicle vehicle = World.GetNearbyVehicles(ped, 40f)
+                     .Where(v => v != null
+                        && v.Exists()
+                        && v.IsDriveable
+                        && !v.IsUpsideDown
+                        && v.WheelType != VehicleWheelType.BikeWheels
+                        && v.IsStopped)
+                    .OrderBy(p => Math.Abs(p.Position.DistanceTo(ped.Position)))
+                    .FirstOrDefault();
+
+            return vehicle;
+        }
+    }
+}
